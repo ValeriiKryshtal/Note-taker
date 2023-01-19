c96e1a0d7575fa7e4ccbd971e33a71a2a6467065
Valerii Kryshtal <112430151+ValeriiKryshtal@users.noreply.github.com>
2023-01-19 05:10:16 -0400

    cleaned and polished the code

--- routes/notes.js
+++ routes/notes.js
@@ -1,20 +1,18 @@
 const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const {
-     readFromFile,
-     readAndAppend,
-     writeToFile,
-} = require('../helpers/fsUtils');
-
-// GET Route for retrieving all the tips
+          readFromFile,
+          readAndAppend,
+          writeToFile,
+                         } = require('../helpers/fsUtils');
+// GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
      readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
-
 // GET Route for a specific note
 notes.get('/:note_id', (req, res) => {
      const noteId = req.params.note_id;
-     console.log(noteId);
+     // console.log(noteId);
      readFromFile('./db/db.json')
      .then((data) => JSON.parse(data))
      .then((json) => {
@@ -24,27 +22,20 @@ notes.get('/:note_id', (req, res) => {
           : res.json('No note with that ID');
      });
 });
-
 // DELETE Route for a specific tip
 notes.delete('/:note_id', (req, res) => {
-  const noteId = req.params.note_id;
-  readFromFile('./db/db.json')
-    .then((data) => JSON.parse(data))
-    .then((json) => {
-      // Make a new array of all notes except the one with the ID provided in the URL
-      const result = json.filter((note) => note.note_id !== noteId);
-
-      // Save that array to the filesystem
-      writeToFile('./db/db.json', result);
-
-      // Respond to the DELETE request
-      res.json(`Item ${noteId} has been deleted 🗑️`);
-    });
+     const noteId = req.params.note_id;
+     readFromFile('./db/db.json')
+          .then((data) => JSON.parse(data))
+          .then((json) => {
+               const result = json.filter((note) => note.note_id !== noteId);   // Make a new array of all notes except the one with the ID provided in the URL            
+               writeToFile('./db/db.json', result);                             // Save that array to the filesystem
+     res.json(`Item ${noteId} has been deleted 🗑️`);                            // Respond to the DELETE request
+     });
 });
-
 // POST Route for a new note
 notes.post('/', (req, res) => {
-     console.log(req.body);
+     // console.log(req.body);
      const { title, text } = req.body;
      if (req.body) {
      const newNote = {
@@ -58,5 +49,4 @@ notes.post('/', (req, res) => {
      res.error('Error in adding note');
      }
 });
-
 module.exports = notes;
